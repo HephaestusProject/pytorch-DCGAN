49ff02c4efc6169354a6183f4e6bd92eadb21262
hyerim1048 <gpflav@gmail.com>
2020-08-08 16:16:31 +0900

    first commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM pytorch/pytorch:1.6.0-cuda10.1-cudnn7-runtime
+ADD requirements.txt /workspace
+RUN pip install -r requirements.txt 
+COPY . /workspace
+RUN python train.py
--- train.py
+++ train.py
@@ -1,10 +1,23 @@
-"""
-    This script was made by Nick at 19/07/20.
-    To implement code for training your model.
-"""
 import pytorch_lightning
 import torch
+from argparse import ArgumentParser
+import omegaconf
+import torchvision
 
-pytorch_lightning.seed_everything(777)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
+
+def get_dataset(dataset: str):
+    return torchvision.datasets.SVHN(split="train")
+
+
+def run(args) -> None:
+    # dataset 을 가져오고
+    print(args.dataset)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--dataset", default="svhn", type=str, help="the name of dataset"
+    )
+    args = parser.parse_args()
+    run(args)
