5a355fb84801362b662bdebafbefde444c64d4d3
hyerim1048 <gpflav@gmail.com>
2020-09-05 17:02:27 +0900

    #2. dataset class

---
+++ src/dataset.py
@@ -0,0 +1,45 @@
+import torch
+import torchvision
+from torch.utils.data import random_split, DataLoader
+import torchvision.transforms as transforms
+
+
+class SVHN:
+    def __init__(self, conf):
+        self.conf = conf.dataset
+        self.model_params = conf.model.params
+        self.dataset = self.load_dataset()
+        self.validation_dataset, self.train_dataset = self.split_dataset()
+
+    def load_dataset(self):
+        return torchvision.datasets.SVHN(
+            self.conf.path.train,
+            split="train",
+            download=True,
+            transform=transforms.ToTensor(),
+        )
+
+    def split_dataset(self):
+        # split by fixed validation size
+        return random_split(
+            self.dataset,
+            [
+                self.conf.params.validation_size,
+                len(self.dataset) - self.conf.params.validation_size,
+            ],
+            generator=torch.Generator().manual_seed(42),
+        )
+
+    def train_dataloader(self):
+        return DataLoader(
+            dataset=self.train_dataset,
+            batch_size=self.model_params.batch_size,
+            shuffle=True,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            dataset=self.validation_dataset,
+            batch_size=self.model_params.batch_size,
+            shuffle=True,
+        )
