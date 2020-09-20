ebb46fd0a0ae26514e69fa74049c02401d6749eb
hyerim1048 <gpflav@gmail.com>
2020-09-20 11:19:08 +0900

    parameter 일부 수정

--- src/model/net.py
+++ src/model/net.py
@@ -33,42 +33,30 @@ class Generator(nn.Module):
                 bias=False,
             ),
             Reshape(-1, self.hparams.num_gen_filters * 8, s_16, s_16),
+            nn.BatchNorm2d(self.hparams.num_gen_filters * 8),
             nn.ReLU(True),
             self._conv_transpose(
-                self.hparams.num_gen_filters * 8,
-                self.hparams.num_gen_filters * 4,
+                self.hparams.num_gen_filters * 8, self.hparams.num_gen_filters * 4,
             ),  # [128, 256, 4, 4]
             self._conv_transpose(
-                self.hparams.num_gen_filters * 4,
-                self.hparams.num_gen_filters * 2,
+                self.hparams.num_gen_filters * 4, self.hparams.num_gen_filters * 2,
             ),  # input[128, 128, 8, 8]
             self._conv_transpose(
-                self.hparams.num_gen_filters * 2,
-                self.hparams.num_gen_filters * 1,
+                self.hparams.num_gen_filters * 2, self.hparams.num_gen_filters * 1,
             ),  # input[128, 64, 16, 16]
             nn.ConvTranspose2d(
-                self.hparams.num_gen_filters * 1,
-                self.hparams.num_channels,
-                5,
-                2,
-                padding=2,
-                output_padding=1,
+                self.hparams.num_gen_filters * 1, self.hparams.num_channels, 4, 2, 1,
             ),
             nn.Tanh(),  # [128, 3, 32, 32]
         )
 
     def _conv_transpose(
-        self, c_in: int, c_out: int, kernel_size: int = 5, stride: int = 2
+        self, c_in: int, c_out: int, kernel_size: int = 4, stride: int = 2
     ) -> nn.Sequential:
         return nn.Sequential(
             nn.ConvTranspose2d(
-                c_in,
-                c_out,
-                kernel_size,
-                stride,
-                padding=2,
-                output_padding=1,
-            ),
+                c_in, c_out, kernel_size, stride, 1, bias=False,
+            ),  # bias false
             nn.BatchNorm2d(c_out),
             nn.ReLU(True),
         )
@@ -96,19 +84,15 @@ class Discriminator(nn.Module):
                 self.hparams.num_d_filters * 4, self.hparams.num_d_filters * 8
             ),  # [128, 512, 2, 2]
             Reshape(-1, self.hparams.num_d_filters * 8 * 2 * 2),
-            nn.Linear(
-                self.hparams.num_d_filters * 8 * 2 * 2,
-                1,
-                bias=False,
-            ),
+            nn.Linear(self.hparams.num_d_filters * 8 * 2 * 2, 1, bias=False,),
             nn.Sigmoid(),
         )
 
     def _conv(
-        self, c_in: int, c_out: int, kernel_size: int = 5, stride: int = 2
+        self, c_in: int, c_out: int, kernel_size: int = 4, stride: int = 2
     ) -> nn.Sequential:
         return nn.Sequential(
-            nn.Conv2d(c_in, c_out, kernel_size, stride, padding=2),
+            nn.Conv2d(c_in, c_out, kernel_size, stride, padding=1, bias=False),
             nn.BatchNorm2d(c_out),
             nn.LeakyReLU(0.2, inplace=True),
         )
